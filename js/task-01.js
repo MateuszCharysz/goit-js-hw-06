f13651d30864e945b83a636227b990e7a5732408
MateuszCharysz <charyszek@interia.pl>
2023-01-24 01:20:15 +0100

    hw-1/10 done

--- js/task-01.js
+++ js/task-01.js
@@ -1,19 +1,19 @@
 'use strict';
 const categoriesList = document.getElementById('categories');
-console.log(categoriesList);
+// console.log(categoriesList);
 const categoryNumberSelector = categoriesList.querySelectorAll('li.item');
-console.log(categoryNumberSelector);
-console.log(categoryNumberSelector.childrenNodes); //undefined - childrenNodes zwraca wartości TYLKO dla pojedynczego elementu, a nie dla grupy
+// console.log(categoryNumberSelector);
+// console.log(categoryNumberSelector.childrenNodes); //undefined - childrenNodes zwraca wartości TYLKO dla pojedynczego elementu, a nie dla grupy
 // const catNumSelArray = [...categoryNumberSelector];
 // console.log(catNumSelArray) // zwraca jako prawdziwy array
 // console.log(catNumSelArray[0]) // zwraca wartość li.item ze wszystkimi pozostałymi elementami we wnętrzu
 // console.log(catNumSelArray[0].childNodes); // tworzy pseudoarray dla wszystkich elementów w POJEDYNECZEJ wartości li.item
 // console.log(categoryNumberSelector[0].childNodes) // również tworzy psudoarray tak sam jak z prawdziwego array
 
-const categoryNames = document.querySelectorAll('h2');
-console.log(categoryNames); // zwraca tylko nagłówki bez tekstu.
-console.log(categoryNames.textContent); // zwraca undefinded - musi mieć pojedyncze elementy.
-console.log(categoryNames[0].textContent); //zwraca tekst pojedynczego nagłówka
+// const categoryNames = document.querySelectorAll('h2');
+// console.log(categoryNames); // zwraca tylko nagłówki bez tekstu.
+// console.log(categoryNames.textContent); // zwraca undefinded - musi mieć pojedyncze elementy.
+// console.log(categoryNames[0].textContent); //zwraca tekst pojedynczego nagłówka
 
 const numberOfCategories = () => {
   let number = categoryNumberSelector.length;
@@ -27,14 +27,14 @@ const namesAndElements = () => {
   let messegeTwo = "";
   catNumSelArray.forEach(listItem => {
     let listItemArray = [...listItem.childNodes];
-    console.log(listItemArray);
-    listItemArray.forEach((simpleElements, index) => {
-      console.log(typeof simpleElements)
-      if (simpleElements == 'h2') {
-        console.log(simpleElements[index].textContent);
-        messegeOne = `Category: ${simpleElements[index].textContent}`
-      } else if (simpleElements == 'ul') {
-       console.log(simpleElements.children.length);
+    // console.log(listItemArray);
+    listItemArray.forEach((simpleElements) => {
+      // console.log(typeof simpleElements)
+      if (simpleElements.localName == 'h2') {
+        // console.log(simpleElements.textContent);
+        messegeOne = `Category: ${simpleElements.textContent}`
+      } else if (simpleElements.localName == 'ul') {
+      //  console.log(simpleElements.children.length);
         messegeTwo = `Elements: ${simpleElements.children.length}`;
       };
     })
