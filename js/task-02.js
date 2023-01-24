76e257b5f4603a4fc9d5862d1d099cf5af7d5bad
MateuszCharysz <charyszek@interia.pl>
2023-01-24 22:52:55 +0100

    hw 2/10

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,26 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+// elem.classList.add(cls);
+const ingredientsList = document.getElementById('ingredients');
+// console.log(ingredientsList)
+// document.createElement();
+// item; class
+
+const makeIngredientsList = () => {
+  let listElementArr = [];
+  ingredients.forEach(ingredient => {
+    // console.log(ingredient)
+    const listElement = document.createElement('li');
+    // console.log(listElement);
+    listElement.textContent = ingredient; //adds content
+    listElement.classList.add('item'); //adds class
+    listElementArr.push(listElement);
+    // ingredientsList.append(listElement);
+  });
+  // console.log(listElementArr)
+  ingredientsList.append(...listElementArr);
+};
+
+makeIngredientsList();
