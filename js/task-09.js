b8933d5949047d4f134923c3b65011cecf3a1b2c
MateuszCharysz <charyszek@interia.pl>
2023-01-29 00:36:12 +0100

    task 9/10 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,22 @@
+'use strict';
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+//FIRST SOLUTION
+//help
+const log = console.log;
+
+//DOM elements
+const btnChange = document.querySelector('.change-color');
+log(btnChange);
+const colorDisplay = document.querySelector('.color');
+
+//callback/functions
+const colorChange = Event => {
+  document.body.style.backgroundColor = `${getRandomHexColor()}`;
+  colorDisplay.innerHTML = document.body.style.backgroundColor;
+};
+
+//Event
+btnChange.addEventListener('click', colorChange);
