e137eb2d7341d35bf1b9a95149f1e7e6ebdd0dcc
MateuszCharysz <charyszek@interia.pl>
2023-01-28 22:58:59 +0100

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,16 @@
+'use strict';
+//FIRST SOLUTION
+//help
+const log = console.log;
 
+//DOM elements
+const form = document.querySelector('.login');
+log(input);
+const inputLength = input.value;
+log(typeof input.getAttribute('data-length'));
+
+//callback/functions
+
+
+//Event
+// input.addEventListener('blur', inputlengthVal);
